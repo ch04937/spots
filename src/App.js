488e728ee9f0829d3f0fa533a3992fc4e39b08cb
ch04937 <97hernandez.c@gmail.com>
2019-11-17 18:23:30 -0800

    forms are components

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { Route } from "react-router-dom";
 import NavBar from "./pages/NavBar.js/index.js";
 import HomePage from "./pages/HomePage.js";
 import Profile from "./pages/Profile.js";
-import Login from "./pages/Login";
-import RegisterForm from "./pages/RegisterForm.js";
+import Login from "./components/Login";
+import RegisterForm from "./components/RegisterForm.js";
 import Footer from "./pages/footer.js/index.js";
 import Chat from "./pages/Chat.js/index.js.js.js.js";
 
