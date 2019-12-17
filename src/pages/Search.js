be0da58db60fb5e4245c8e4f9eb503f375df53d8
ch04937 <97hernandez.c@gmail.com>
2019-12-17 11:08:41 -0800

    not more chats

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,17 +1,12 @@
 import React, { useEffect, useContext } from "react";
 // import { Route, Link } from "react-router-dom";
-import SideNav from "@trendmicro/react-sidenav"; // { NavItem, NavText }
 
 //adding components
-// import MessagesList from "../components/MessagesList";
-import SendMessageForm from "../components/SendMessageForm";
-
 import { AuthContext } from "../context/auth/authState";
 import { TwhereContext } from "../context/twhere/twhereState";
 
 //setting styles
 import "./pages.scss";
-import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 
 const Search = props => {
 	const { userProfile } = useContext(AuthContext);
@@ -21,17 +16,7 @@ const Search = props => {
 	}, []);
 	return (
 		<div className="search-container">
-			<div className="search-menu">
-				<React.Fragment>
-					<SideNav>
-						<SideNav.Toggle />
-					</SideNav>
-				</React.Fragment>
-			</div>
-			<div className="search-body">
-				<p>{userProfile.username}</p>
-				<SendMessageForm />
-			</div>
+			<h3>Twhere to today? {userProfile.username}</h3>
 		</div>
 	);
 };
